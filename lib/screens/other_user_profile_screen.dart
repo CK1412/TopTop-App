4d4286abef51869c19d65939578bef2f6d63d926
Tran Huy Canh <taikhoanhoctap48693@gmail.com>
2022-05-22 09:47:38 +0700

    reset git cache of project

---
+++ lib/screens/other_user_profile_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class OtherUserProfileScreen extends ConsumerStatefulWidget {
+  const OtherUserProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _OtherUserProfileScreenState();
+}
+
+class _OtherUserProfileScreenState
+    extends ConsumerState<OtherUserProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
